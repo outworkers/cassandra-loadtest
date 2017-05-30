657662d020db799d7d3f3fe92bbe7f985e626e31
Flavian Alexandru <flavian@outworkers.com>
2017-05-30 15:06:01 +0100

    Removing more dead code

--- app/db/model/GroupId.scala
+++ app/db/model/GroupId.scala
@@ -1,20 +1,11 @@
 package db.model
 
 import java.util.{Date, UUID}
-
-import org.joda.time.DateTime
 import play.api.libs.json._
-import _root_.util.Util
 
 case class GroupId(groupId: UUID, id: UUID, createTs: Date)
 
 object GroupId {
 
-  implicit val readsJodaLocalDateTime = Reads[DateTime](js =>
-    js.validate[String].map[DateTime](dtString =>
-      DateTime.parse(dtString, Util.fmt)
-    )
-  )
-
-  implicit val groupIdFormat = Json.format[GroupId]
+  implicit val groupIdFormat: OFormat[GroupId] = Json.format[GroupId]
 }
